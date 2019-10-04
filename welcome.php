8b88eb9dd87348994e24a56a6146659ff9d970dd
configuroweb <msevillab@gmail.com>
2019-10-04 08:19:27 -0500

    Optimización del login
    Se incluye enlace tips

--- welcome.php
+++ welcome.php
@@ -22,10 +22,15 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <div class="page-header">
         <h1>Hola, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenid@ a nuestro sitio.</h1>
+        
     </div>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Cambia tu contraseña</a>
         <a href="logout.php" class="btn btn-danger">Cierra la sesión</a>
+        <br><br><h2>Clic en el logo para más tips <a href="https://www.configuroweb.com/desarrollo/"><img src="images/configuroweb.png" width="170"></a>
+        <br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLEp4kajrb4a0fgy8e5JO3iGO7iKnPwQFQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </p>
+
+
 </body>
 </html>
